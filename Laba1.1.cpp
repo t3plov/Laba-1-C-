f0efddef3229b6f853a0fc1cdf84f876baeb3209
t3plov <152847875+t3plov@users.noreply.github.com>
2024-02-12 09:44:51 +0300

    Add files via upload

---
+++ Laba1.1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <cmath>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+	float x;
+	const float pi = 3.14;
+
+	cout << "Enter x: " << endl;
+	cin >> x;
+	if (cin.fail())
+	{
+		cout << "Incorect input" << endl;
+
+		return 1;
+	}
+	
+	float Sq_of_circle = pow(pi, 2) * x;
+	float Sq_of_square = pow(x, 2) / 2;
+	
+	cout << "Total square (x = "<< x << ") : " << Sq_of_circle - Sq_of_square << endl;
+
+	return 0;
+}
